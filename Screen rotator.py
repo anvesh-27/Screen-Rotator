43447745656c0df538bc84622c4d85bfd94e3b7c
Anvesh Katiyar <69871258+anvesh-27@users.noreply.github.com>
2021-07-04 21:50:34 +0530

    Update Screen rotator.py

--- Screen rotator.py
+++ Screen rotator.py
@@ -1,6 +1,6 @@
 #Pls check the Readme file before using it there you will find ehich module you need to install and everything else.
-import rotatescreen
+import rotatescreen #pip install rotate-screen
 
 screen = rotatescreen.get_primary_display()
 
-screen.rotate_to(0)#replace 0 with any angle as 90, 180 or 270
+screen.rotate_to(0) #replace 0 with any angle as 90, 180 or 270 at which you want your screen to be rotated
